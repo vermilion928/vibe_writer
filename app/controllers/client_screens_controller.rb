826af834d451c3915dc0c3c3e3c0d36b7932a584
akai <vermilion928@gmail.com>
2016-04-04 01:55:06 +0900

    Modified client_screens button design.

--- app/controllers/client_screens_controller.rb
+++ app/controllers/client_screens_controller.rb
@@ -1,6 +1,6 @@
 class ClientScreensController < ApplicationController
   def show
-    @chara   = [*'a'..'z'][rand 26]
+    @chara   = [*'a'..'z'].sample
     @hue_num = rand 360
   end
 end
--- app/controllers/standby_screens_controller.rb
+++ app/controllers/standby_screens_controller.rb
@@ -1,12 +1,5 @@
 class StandbyScreensController < ApplicationController
-  before_action :redirect_mobile_view
-
   def show
-  end
-
-  private
-
-  def redirect_mobile_view
-    redirect_to client_screen_path if request.user_agent =~ /iPhone|iPad|Android/
+    return redirect_to client_screen_path if request.user_agent =~ /iPhone|iPad|Android/
   end
 end
